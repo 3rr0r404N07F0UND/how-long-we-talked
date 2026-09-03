41b33d68d1c7b440535d60c2117d01e354b499c4
3rr0r404N07F0UND <117527664+3rr0r404N07F0UND@users.noreply.github.com>
2026-09-03 23:25:25 +0900

    add datetime

--- main.r
+++ main.r
@@ -1,9 +1,9 @@
 source("./total_format_time.r")
 source("./format_time.r")
 
-raw_data <- read.csv("phone.txt", fileEncoding = "UTF-8")
+raw_data <- read.csv("phone.csv", fileEncoding = "UTF-8")
 
-raw_data_seconds <- sapply(raw_data$time, function(raw_time) {
+raw_data_seconds <- sapply(raw_data$duration, function(raw_time) {
   temp_time <- strsplit(raw_time, ":")
   hour <- as.numeric(temp_time[[1]][1])
   minute <- as.numeric(temp_time[[1]][2])
@@ -14,7 +14,7 @@ raw_data_seconds <- sapply(raw_data$time, function(raw_time) {
 
 result_list <- list()
 
-result_list$length <- length(raw_data$time)
+result_list$length <- length(raw_data$duration)
 result_list$total_seconds <- sum(raw_data_seconds)
 result_list$total_minutes <- result_list$total_seconds %/% 60
 result_list$total_hours <- result_list$total_minutes %/% 60
@@ -25,9 +25,27 @@ result_list$median_seconds <- median(raw_data_seconds)
 result_list$max_seconds <- max(raw_data_seconds)
 result_list$min_seconds <- min(raw_data_seconds)
 
+dates <- as.Date(raw_data$datetime)
+date_counts <- table(dates)
+result_list$most_call_date <- names(date_counts)[which.max(date_counts)]
+result_list$most_call_count <- max(date_counts)
+
+daily_seconds <- tapply(
+  raw_data_seconds,
+  dates,
+  sum
+)
+
+result_list$most_call_time_date <- names(daily_seconds)[which.max(daily_seconds)]
+result_list$most_call_time <- max(daily_seconds)
+
 cat(sprintf("총 통화 횟수: %d회", result_list$length), end = "\n")
 cat(total_format_time(total_seconds = result_list$total_seconds), end = "\n")
 cat(sprintf("평균 통화 시간: %s (회당)", format_time(as.integer(result_list$average_seconds))), end = "\n")
 cat(sprintf("중앙값(중위수): %s", format_time(as.integer(result_list$median_seconds))), end = "\n")
 cat(sprintf("가장 긴 통화: %s", format_time(as.integer(result_list$max_seconds))), end = "\n")
 cat(sprintf("가장 짧은 통화: %s", format_time(as.integer(result_list$min_seconds))), end = "\n")
+cat(sprintf("가장 전화 많이 한 날: %s (%d회)", result_list$most_call_date, result_list$most_call_count), end = "\n")
+cat(sprintf(
+  "가장 오래 통화한 날: %s (%s)", result_list$most_call_time_date, format_time(result_list$most_call_time)
+), end = "\n")
